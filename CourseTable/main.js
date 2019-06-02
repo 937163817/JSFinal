5e8da6f16f1f32667532e16a70de7839add5922d
yun <jiangyun0430@gmail.com>
2019-06-02 20:15:17 +0800

    modify ufo

--- CourseTable/main.js
+++ CourseTable/main.js
@@ -5,7 +5,7 @@ window.onload=function()
 
 $(document).ready(function(){
 
-    $("#courseTable").append("<tr><th>場次</th><th>時間</th><th>主題</th><tr>");
+    $("#courseTable").append("<tr><th>排序</th><th>距離(百萬公里)</th><th>行星</th><tr>");
     
     var topicCount=topic.length;
     
@@ -18,8 +18,9 @@ $(document).ready(function(){
     {
         $("#courseTable").append("<tr>");
         $("#courseTable").append("<td>"+(x+1)+"</td>");
-        $("#courseTable").append("<td>"+
-            (new Date(startDate.getTime()+x*7*dayUnit)).toLocaleDateString().slice(5)+"</td>");
+//        $("#courseTable").append("<td>"+
+//            (new Date(startDate.getTime()+x*7*dayUnit)).toLocaleDateString().slice(5)+"</td>");
+        $("#courseTable").append("<td>"+distan[x]+"</td>");
         $("#courseTable").append("<td>"+topic[x]+"</td>");
         $("#courseTable").append("<tr>");
     }
